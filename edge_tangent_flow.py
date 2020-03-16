e2963560a046ddd9d1dbae4b4237f04fd70a6952
Pablo Eliseo <pablo.7.pg@gmail.com>
2020-03-16 20:06:41 +0100

    made with classes

--- edge_tangent_flow.py
+++ edge_tangent_flow.py
@@ -2,73 +2,79 @@
 Coherent Line Drawing - Pablo Eliseo
 """
 #Edge Tangent Flow
+import sys
 import math
 import cv2
 import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
+
+################# Classes ###################
+class ETF:
+    """Main class, Edge Tangent Flow"""
+    def __init__(self, file):
+        self.__kernel = 5
+        img = cv2.imread(str(file), 0) #0 for grayscale image
+        self.shape = img.shape+(2,)
+        self.flow_field = np.zeros(self.shape, dtype=np.float32)
+        img_n = cv2.normalize(img.astype("float32"), None, 0.0, 1.0, cv2.NORM_MINMAX)
+
+        #Generate partial derivatives
+        x_der, y_der = [], []
+        x_der = cv2.Sobel(img_n, cv2.CV_32FC1, 1, 0, ksize=self.__kernel) #kernel size = 5
+        y_der = cv2.Sobel(img_n, cv2.CV_32FC1, 0, 1, ksize=self.__kernel) #kernel size = 5
+
+        #Compute gradient
+        self.gradient_mag = cv2.sqrt(x_der**2.0 + y_der**2.0)
+        self.gradient_mag = cv2.normalize(self.gradient_mag.astype("float32"), None, 0.0, 1.0, cv2.NORM_MINMAX)
+
+        h, w = img.shape[0], img.shape[1]
+        for i in range(h):
+            for j in range(w):
+                a = x_der[i][j]
+                b = y_der[i][j]
+                vect = np.array([a, b], ndmin=1)
+                cv2.normalize(vect.astype("float32"), vect, 0.0, 1.0, cv2.NORM_MINMAX)
+                self.flow_field[i][j] = compute_tangent(vect)
+    
+    
+    def compute_ETF(self):
+        tangent_new = np.zeros(self.shape, dtype=np.float32)
+        h, w = self.shape[0], self.shape[1]
+        for x in range(h):
+            for y in range(w):
+                tangent_new[x][y] = self.__compute_new_etf(x, y)
+        self.flow_field = tangent_new
+    
+    def save(self):
+        np.save("EFT.npy", self.flow_field)
+        np.save("gradient_magnitude.npy", self.gradient_mag)
+
+    ## PRIVATE METHODS ##
+    def __compute_new_etf(self, x, y):
+        t_new = np.zeros(2, dtype=np.float32)
+        h, w = self.flow_field.shape[0], self.flow_field.shape[1]
+        for c in range(x - self.__kernel, x + self.__kernel + 1):
+            for r in range(y - self.__kernel, y + self.__kernel + 1):
+                x_tan = self.flow_field[x][y]
+                if (r < 0 or r >= w or c < 0 or c >= h):
+                    continue
+                y_tan = self.flow_field[c][r]
+                a = np.array([x, y])
+                b = np.array([c, r])
+                phi = compute_phi(x_tan, y_tan)
+                ws = compute_ws(a, b, self.__kernel)
+                wm = compute_wm(self.gradient_mag[x][y], self.gradient_mag[c][r])
+                wd = compute_wd(x_tan, y_tan)
+                t_new += phi * y_tan * ws * wm * wd
+        return t_new
+
 #######################   AUX FUNCTIONS   ##########################
 def compute_tangent(v):
+    # if v = (v[0],v[1]), v_perpendicular = [-v[0], v[1]] 
     theta = math.pi/2 #90º
-    #rot matrix 90º
-    x = v[0] * np.cos(theta) - v[1] * np.sin(theta)
-    y = v[1] * np.cos(theta) + v[0] * np.sin(theta)
+    x = v[0] * np.cos(theta) - v[1] * np.sin(theta) #-v[1]
+    y = v[1] * np.cos(theta) + v[0] * np.sin(theta) # v[0]
     return [x, y]
-
-
-####################################################################
-######################    PAPER'S FUNCTIONS    #####################
-####################################################################
-
-# t_0 (0) no equation, specified at the end of the section
-def initial_etf(img):
-    tangent = np.zeros(img.shape + (2,), dtype=np.float32)
-    img_n = cv2.normalize(img.astype("float32"), None, 0.0, 1.0, cv2.NORM_MINMAX)
-
-    #Generate gradients
-    x_grad, y_grad = [], []
-    x_grad = cv2.Sobel(img_n, cv2.CV_32FC1, 1, 0, ksize=5) #kernel size = 5
-    y_grad = cv2.Sobel(img_n, cv2.CV_32FC1, 0, 1, ksize=5) #kernel size = 5
-
-    #Compute gradient
-    gradient_mag = cv2.sqrt(x_grad**2.0 + y_grad**2.0)
-    gradient_mag = cv2.normalize(gradient_mag.astype("float32"), None, 0.0, 1.0, cv2.NORM_MINMAX)
-
-    h, w = img.shape[0], img.shape[1]
-    for i in range(h):
-        for j in range(w):
-            a = x_grad[i][j]
-            b = y_grad[i][j]
-            vect = np.array([a, b], ndmin=1)
-            cv2.normalize(vect.astype("float32"), vect, 0.0, 1.0, cv2.NORM_MINMAX)
-            tangent[i][j] = compute_tangent(vect)
-    return tangent, gradient_mag
-
-# t_new for every pixel, no equation
-def compute_image_etf(tangent, kernel, gradient_mag):
-    tangent_new = np.zeros(tangent.shape, dtype=np.float32)
-    h, w = tangent.shape[0], tangent.shape[1]
-    for x in range(h):
-        for y in range(w):
-            tangent_new[x][y] = compute_new_etf(x, y, tangent, kernel, gradient_mag)
-    return tangent_new
-   
-# t_new (1)
-def compute_new_etf(x, y, tangent, kernel, gradient_mag):
-    t_new = np.zeros(2, dtype=np.float32)
-    h, w = tangent.shape[0], tangent.shape[1]
-    for c in range(x - kernel, x + kernel + 1):
-        for r in range(y - kernel, y + kernel + 1):
-            x_tan = tangent[x][y]
-            if (r < 0 or r >= w or c < 0 or c >= h):
-                continue
-            y_tan = tangent[c][r]
-            a = np.array([x, y])
-            b = np.array([c, r])
-            phi = compute_phi(x_tan, y_tan)
-            ws = compute_ws(a, b, kernel)
-            wm = compute_wm(gradient_mag[x][y], gradient_mag[c][r])
-            wd = compute_wd(x_tan, y_tan)
-            t_new += phi * y_tan * ws * wm * wd
-    return t_new
 
 # ws (2)
 def compute_ws(x_vect, y_vect, radius):
@@ -76,7 +82,7 @@ def compute_ws(x_vect, y_vect, radius):
         return 1
     return 0
 
- # wm (3)
+# wm (3)
 def compute_wm(x_grad_norm, y_grad_norm):
     eta = 1 #Specified in paper
     return (1/2) * (1 + np.tanh(eta*(x_grad_norm - y_grad_norm)))
@@ -91,16 +97,14 @@ def compute_phi(x_tan, y_tan):
         return 1
     return -1
 
+
 ####################################################################
 #########################  MAIN FUNCTION  ##########################
 ####################################################################
 
 if __name__ == '__main__':
-    KERNEL = 5
-    IMG = cv2.imread("prueba1.jpg", 0) #0 for grayscale image
-    TANGENT, GRADIENT_MAG = initial_etf(IMG)
-    ITERATIONS = 1
-    for it in range(ITERATIONS):
-        TANGENT = compute_image_etf(TANGENT, KERNEL, GRADIENT_MAG)
-    print(TANGENT)
-    
+    INSTANCE = ETF(sys.argv[1])
+    for i in range(int(sys.argv[2])):
+        INSTANCE.compute_ETF()
+    INSTANCE.save()
+    print(INSTANCE.flow_field)
